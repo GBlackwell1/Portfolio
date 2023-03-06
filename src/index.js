3be3c2455fd741f2f3e9d61a0a86989b4fb61019
Gabriel Blackwell <gabrielsblackwell@gmail.com>
2023-03-06 10:54:57 -0600

    all files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+// Allows to hook react to html
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// What this does is in the index.html doc you just render the entire
+// app, (which you already know is the root to the react project)
+// inside this divider with the Id 'root', super duper easy
+ReactDOM.render(<App/>, document.getElementById('root'))
