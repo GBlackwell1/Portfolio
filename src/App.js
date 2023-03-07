d13bb8825980668bb93c9bcd5a907c607dfebdb7
Gabriel Blackwell <gabrielsblackwell@gmail.com>
2023-03-06 19:05:58 -0600

    File structure for container and index files complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Article, Brand, CTA, Feature, Navbar} from "./components";
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './container';
+import { CTA, Brand, Navbar } from './components';
 
 const App = () => {
   return (
