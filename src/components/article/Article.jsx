b7c49f57296205905d88a10077f7417d4d5e5042
Gabriel Blackwell <gabrielsblackwell@gmail.com>
2023-03-06 18:11:51 -0600

    Index

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./article.css";
+
+const Artcile = () => {
+  return (
+    <div>Artcile</div>
+  )
+}
+
+export default Artcile
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// Allows us to expot all as one file instead of exporting
+// each individua component into app
+export { default as Article } from "./article/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as CTA } from "./cta/CTA";
+export { default as Feature } from "./feature/Feature";
+export { default as Navbar } from "./navbar/Navbar";
