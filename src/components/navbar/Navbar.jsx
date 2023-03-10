3bb02466f25a41f7aee09bceb0ff8237d74fc2c0
Gabriel Blackwell <gabrielsblackwell@gmail.com>
2023-03-09 18:45:18 -0600

    Navbar work done, mobile done, header in progress

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
+import { useState } from 'react';
 import "./navbar.css";
-import { RiMenu3Line ,RiCloseLin } from 'react-icons/ri';
+import { RiMenu3Line ,RiCloseLine } from 'react-icons/ri';
 
+// Wrapped with react fragment
+const Menu = () => (
+  <>  
+  <p>< a href="#home">Home</a></p>
+  <p>< a href="#experience">Experience</a></p>
+  <p>< a href="#ongoingprojects">Ongoing Projects</a></p>
+  <p>< a href="#education">Education</a></p>
+  <p>< a href="#blog">Blog</a></p>
+  </>        
+)
 // BEM -> Block Element Modifier
 // naming convention
 
 const Navbar = () => {
+  const[toggleMenu, setToggleMenu] = useState(false);
+  
   return (
-    <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links">
-        <div className="gptr__navbar-links_logo">
-          <img src={logo} alt="logo" />
+    <div className="portfolio__navbar">
+      <div className="portfolio__navbar-links">
+      <p className="portfolio__navbar-links_name">Gabriel Blackwell</p>
+        <div className="portfolio__navbar-links_container">
+          <Menu />
         </div>
+      </div>
+      <div className="portfolio__navbar-menu">
+        {/* SYNTAX FOR CALL COULD BE WRONG TODO: */}
+        {toggleMenu 
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="portfolio__navbar-menu_container scale-up-center">
+            <div className="portfolio__navbar-menu_container-links ">
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import "./header.css"
+import portrait from "../../assets/me.png";
+import GitCat from "../../assets/githubcat.png";
+import GitWord from "../../assets/githubword.png";
+import YouTube from "../../assets/youtube.png";
+import LinkedIn from "../../assets/linkedin.png";
+/* Put back in new portrait when you can */
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="portfolio__header section__padding" id="home">
+      <div className="portfolio__header-content">
+        <h1 className="gradient__text">
+          Who is Gabriel Blackwell?
+        </h1>
+        <p>
+          I am a Computer Engineering student at University of South Carolina, 
+          which belongs in the top 100 public universities in the US for engineering. 
+          My focus over the past year has been has been research outside of academics. 
+          My main focus is developing projects in the realm of Human-Computer Interactions 
+          and Human-Robot Interactions.
+        </p>
+        <div className="portfolio__header-links">
+          <div>
+            <img src={GitCat} alt="GitCat" className="portfolio__header-links-git"/>
+            <img src={GitWord} alt="GitWord" className="portfolio__header-links-gitword"/>
+          </div>
+          <div className="portfolio__header-links-yt">
+            <img src={YouTube} alt="YouTube" />
+          </div>
+          <div className="portfolio__header-links-lin">
+            <p>Linked</p>
+            <img src={LinkedIn} alt="LinkedIn" />
+          </div>
+        </div>
+      </div>
+      <div className="portfolio__header-image">
+        <img src={portrait} alt="portrait" />
+      </div>
+    </div>
   )
 }
 
